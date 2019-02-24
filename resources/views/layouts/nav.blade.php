9e6a755f329f58413e67710fc1e483d85aed66de
Lukas Sinkar <lukassinkarofficial@gmail.com>
2019-02-24 02:58:12 +0200

    changed app name

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4 sticky-top">
-    <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
+    <a class="navbar-brand" href="/">Help Desk</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
         aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
