6822c2d92395dcd1509e49fa94754596e8d08b7e
Lukas Sinkar <lukassinkarofficial@gmail.com>
2019-02-24 02:09:59 +0200

    updated

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'Help Desk') }}</title>
-        {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+        <script src="{{ asset('js/app.js') }}" defer></script>
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
